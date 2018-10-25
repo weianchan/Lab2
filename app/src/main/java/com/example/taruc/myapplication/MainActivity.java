930bf141d1c36e736c638defba905fea8708fc31
anreh <anurehka@gmail.com>
2018-10-25 13:46:07 +0800

    2.1 task 3

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.taruc.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("MYOWN", "This is a debug level log");
         Log.w("BLABBLAB", "This is a warning level log");
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        Log.d(LOG_TAG, "Button clicked!");
+        startActivity(intent);
     }
 }
